1af6ad02d4d9a8b81dad780925e4318dd323167a
Ajay Ingole <ajayingole44@gmail.com>
2025-08-30 20:16:27 +0530

    new package created and one class file

---
+++ src/test/java/testpackage/Test2.java
@@ -0,0 +1,12 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void b() {
+		System.out.println("second class file created ");
+		System.out.println("hello test 2  ");
+	}
+}
