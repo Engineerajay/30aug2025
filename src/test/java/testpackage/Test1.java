d2619735fa1eb3ab131ae17bb22b0da85a3928bc
Ajay Ingole <ajayingole44@gmail.com>
2025-08-30 16:13:10 +0530

    first maven project

---
+++ src/test/java/testpackage/Test1.java
@@ -0,0 +1,20 @@
+package testpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 {
+
+	@Test
+	public void a() {
+//		WebDriverManager.edgedriver().setup();
+//		WebDriver driver=new EdgeDriver();
+//		driver.get("https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager/6.2.0");
+		System.out.println("Hello github");
+		System.out.println("hello ");
+	}
+}
